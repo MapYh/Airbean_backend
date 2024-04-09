7fa3f4a10688475e24ffa7c935c151021cfcdaf8
MapYh <145336421+MapYh@users.noreply.github.com>
2024-04-09 15:44:53 +0200

    Uppdaterade koden med en get för about sidan.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const Datastore = require("nedb-promise")
-
+const Datastore = require("nedb-promise");
 
 //Init app
 const app = express();
@@ -13,30 +12,44 @@ const PORT = 8000;
 const URL = "127.0.0.1";
 const menuData = require("./model/menu.json");
 
-
 //init en ny databas
-const beansMenu = new Datastore({filename: "./model/menu.db", autoload: true});
+const beansMenu = new Datastore({
+  filename: "./model/menu.db",
+  autoload: true,
+});
 
+// data för about sidan.
+const aboutText = new Datastore({
+  filename: "./model/about.db",
+  autoload: true,
+});
 
 /*--------------GET--------------- */
 app.get("/", (req, res) => {
-  res.status(200).json({message: "testing"});
+  res.status(200).json({ message: "testing" });
 });
 
 app.get("/beans", async (req, res) => {
-  
   try {
-    
     const beans = await beansMenu.find({});
-    //Vet ej hur han vill att vi hämtar datan. Alltingen så eller så bytes "menuData" ut mot "beans" 
+    //Vet ej hur han vill att vi hämtar datan. Alltingen så eller så bytes "menuData" ut mot "beans"
     res.status(201).json(menuData);
-    
   } catch (error) {
-    res.status(500).json({message: "internal server error!"})
+    res.status(500).json({ message: "internal server error!" });
   }
-})
+});
 
 app.get("/user/product", (req, res) => {});
+
+//Hämtar all data för en about sida.
+app.get("/about", async (req, res) => {
+  try {
+    const aboutInformation = await aboutText.find({});
+    res.status(201).json(aboutInformation);
+  } catch (error) {
+    res.status(500).json({ message: "internal server error!" });
+  }
+});
 
 app.listen(PORT, URL, () => {
   console.log(`Server running on: ${URL}:${PORT}`);
