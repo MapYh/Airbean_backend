1e1a7b048f7eb8ce645d82fa11aae06e11a5b380
MapYh <145336421+MapYh@users.noreply.github.com>
2024-04-09 14:40:59 +0200

    rand commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import Datastore from "nedb";
 
 //Init app
 const app = express();
+
+let db = new Datastore({ filename: "/menu.json", autoload: true });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 /*--------------Middleware--------------- */
@@ -18,7 +21,11 @@ const URL = "127.0.0.1";
 /*--------------GET--------------- */
 app.get("/", (req, res) => {});
 
-app.get("/user/product", (req, res) => {});
+app.get("/products", (req, res) => {
+  const products = req.body;
+  db.insert(products);
+  res.send(products);
+});
 
 app.listen(PORT, URL, () => {
   console.log(`Server running on: ${URL}:${PORT}`);
