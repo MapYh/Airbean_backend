8b0f6791fed4dff28ec9ae4cabc6583e7c828f07
cozyhorse <mo.apiban@gmail.com>
2024-04-12 16:10:16 +0200

    changed a bug with login

--- login.js
+++ login.js
@@ -5,14 +5,8 @@ const login = express();
 
 login.use(express.json());
 
-const usersData = new Datastore({filename: "./model/users.db", autoload: true});
+const usersData = Datastore({filename: "./model/users.db", autoload: true});
 
-const users = [
-    {email: "horseman@hotmale.com", password: "horse", id: 2, orders: [], name: "Horse"},
-    {email: "catman@hotmale.com", password: "cat", id: 3, orders: [], name: "Cat"},
-    {email: "dogman@hotmale.com", password: "dog", id: 1, orders: [], name: "Dog"},
-
-]
 
 login.get("/users", async (req, res) => {
     try {
@@ -28,11 +22,11 @@ login.get("/users", async (req, res) => {
 login.post("/login", async (req, res) => {
     try {
         const {email, password} = req.body
-        const user = await users.find(user => user.email === email && user.password === password);
+        const user = await usersData.findOne({email: email, password: password});
         if(!user){
             return res.status(404).json({ message: "Login attempt failed"})
         }
-        return res.status(200).json({ message: `Welcome Mr ${user.name}` });
+        return res.status(200).json({ message: `Welcome Mr ${user.name}. Your ID is ${user._id}` });
 
     } catch (error) {
         res.status(500).json({ message: "internal server error!" });
