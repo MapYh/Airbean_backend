907e15373d9deccdd30e8ba763ef7674c90ab2e2
MapYh <145336421+MapYh@users.noreply.github.com>
2024-04-13 00:27:18 +0200

    Added some validation to order.js

--- order.js
+++ order.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const Datastore = require("nedb-promise");
+/* const { v4: uuidv4 } = require("uuid"); */
+const { body, validationResult, checkSchema } = require("express-validator");
 
 const order = express();
-
 order.use(express.json());
 
 const orderData = new Datastore({
@@ -24,23 +25,43 @@ order.get("/orders", async (req, res) => {
   }
 });
 
-order.post("/:id/orders", async (req, res) => {
-  const id = req.params.id;
-  const { orderId, order } = req.body;
-  const newOrder = { orderId, order };
-  console.log("1", newOrder);
-  const foundUser = await userData.findOne({ _id: id });
-  console.log("2", foundUser);
-  try {
-    foundUser.orders.push(newOrder);
+order.post(
+  "/:id/orders",
+  //Validering av request input kollar så att all data har rätt format.
+  body("orderId").isString(),
+  body("orders.*.id").isInt(),
+  body("orders.*.title").isString(),
+  body("orders.*.desc").isString(),
+  body("orders.*.price").isInt(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log("Errors: ", errors);
+    //validering.
+    /* const validatedBody = await Object.keys(req.body).filter(
+      (key) => !["orderId", "orders"].includes(key)
+    );
+    console.log("Validated", orders[0].id);
+    if (validatedBody.length > 0) {
+      return res.status(404).json({ message: "body contained wrong values" });
+    } */
 
-    userData.update({ _id: id }, { $set: { orders: foundUser.orders } });
-    orderData.insert(newOrder);
-    console.log(userData);
-    res.status(201).json("true");
-  } catch (error) {
-    res.status(500).json({ message: "internal server error!" });
+    const id = req.params.id;
+    const { orderId, orders } = req.body;
+    const newOrder = { orderId, orders };
+
+    ///----------------------------
+
+    const foundUser = await userData.findOne({ _id: id });
+    try {
+      foundUser.orders.push(newOrder);
+
+      userData.update({ _id: id }, { $set: { orders: foundUser.orders } });
+      orderData.insert(newOrder);
+      res.status(201).json("true");
+    } catch (error) {
+      res.status(500).json({ message: "internal server error!" });
+    }
   }
-});
+);
 
 module.exports = order;
