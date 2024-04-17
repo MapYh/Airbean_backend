1f4c4c7a534a6c8481efae5708e642b6e6e6aeb6
Ahmed Abbas <ahmed_abbas87@hotmail.com>
2024-04-17 19:34:56 +0200

    flyttade Beans till en egen fil

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { aboutText } = require("./database.js"); // justera sökvägen efter behov
+const { aboutText } = require("./database.js");
 
 router.get("/", async (req, res) => {
   try {
--- index.js
+++ index.js
@@ -1,39 +1,31 @@
 const express = require("express");
-const { beansMenu, aboutText } = require("./database.js");
+const { aboutText } = require("./database.js"); // Om aboutText används någonstans, annars kan du ta bort det här också.
 const login = require("./login.js");
 const signup = require("./signup.js");
 const order = require("./order.js");
 const about = require("./about.js");
+const beans = require('./beans.js');
 
-//Init app
+// Init app
 const app = express();
+
 /*--------------Middleware--------------- */
 app.use(express.json());
-app.use("/auth", login); //använd http://127.0.0.1:8000/auth/"endpoint"
-app.use("/join", signup); //använd http://127.0.0.1:8000/join/"endpoint"
-app.use("/order", order); //för att komma åt använd http://127.0.0.1:8000/orders/"endpoint"
-app.use("/about", about); //för att komma åt använd http://127.0.0.1:8000/about/"endpoint"
-/*--------------Variables--------------- */
+app.use("/auth", login); // använd http://127.0.0.1:8000/auth/"endpoint"
+app.use("/join", signup); // använd http://127.0.0.1:8000/join/"endpoint"
+app.use("/order", order); // för att komma åt använd http://127.0.0.1:8000/orders/"endpoint"
+app.use("/about", about); // för att komma åt använd http://127.0.0.1:8000/about/"endpoint"
+app.use('/beans', beans); // för att komma åt använd http://127.0.0.1:8000/beans/"endpoint"
 
+/*--------------Variables--------------- */
 const PORT = 8000;
 const URL = "127.0.0.1";
-const menuData = require("./model/menu.json");
 
-//init en ny databas
+// Init en ny databas
 
 /*--------------GET--------------- */
 app.get("/", (req, res) => {
   res.status(200).json({ message: "testing" });
-});
-
-app.get("/beans", async (req, res) => {
-  try {
-    const beans = await beansMenu.find({});
-    //Vet ej hur han vill att vi hämtar datan. Alltingen så eller så bytes "menuData" ut mot "beans"
-    res.status(201).json(menuData);
-  } catch (error) {
-    res.status(500).json({ message: "internal server error!" });
-  }
 });
 
 app.get("/user/product", (req, res) => {});
